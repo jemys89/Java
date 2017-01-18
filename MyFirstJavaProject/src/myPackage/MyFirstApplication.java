1b313f67c5bab8d73773e0b64796cecfdf80e3e7
jemys89 <jemys89@gmail.com>
2017-01-18 11:45:10 +0100

    Ejercicio libro Java

---
+++ MyFirstJavaProject/src/myPackage/MyFirstApplication.java
@@ -0,0 +1,9 @@
+package myPackage;
+
+public class MyFirstApplication {
+		public static void main(String[] args){
+
+		System.out.println("I've coded, compiled, and run my first Java program!");
+
+		}
+}
