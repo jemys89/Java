5a9c56dd32978beb759bcbfa8f95fed0ec775b3a
jemys89 <jemys89@gmail.com>
2017-01-24 13:15:29 +0100

    Media aritmética

---
+++ iniciacion_java/ejercicio16/MediaAritmetica.java
@@ -0,0 +1,16 @@
+package org.foobarspam.ejercicio16;
+
+public class MediaAritmetica {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int total = 0;
+		for (int i=0; i<31; i++){
+			System.out.println(i);
+			total = total + i;
+			
+		}
+		System.out.println("La media de aritmética de el 1 al 30 es " + (total/30));
+	}
+
+}
