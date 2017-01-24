c427d4644202f1ed7b9d66d02597b6add2fa9434
jemys89 <jemys89@gmail.com>
2017-01-24 11:41:27 +0100

    Ejercicio para aprender el bucle for

---
+++ ejercicio15/MostrarPorPantalla.java
@@ -0,0 +1,11 @@
+/*#Escribe un programa que escriba cinco veces el mensaje estamos estudiando metodología de la programación.*/
+package org.foobarspam.ejercicio15;
+
+public class MostrarPorPantalla {
+	public static void main(String[] argumentos){
+		for( int i = 0; i < 5; i++ ){
+			System.out.println("Estamos estudiando metodología de la programación.");
+		}
+	}
+
+}
