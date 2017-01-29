c05da02b9e6d93e2b4e3353ad3911fa4528e3d9b
jemys89 <jemys89@gmail.com>
2017-01-29 21:47:49 +0100

    Pendiente de refactorizar

---
+++ Poo/hora/HoraMain.java
@@ -0,0 +1,12 @@
+package org.foobarspam.hora;
+
+public class HoraMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hora prueba = new Hora();
+		prueba.setHora(12,55,55);
+		System.out.println(prueba.imprimirHora());
+	}
+
+}
